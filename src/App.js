4416c76ebf42ebcf17c28bd0cdb3d6028fe95dc4
mikias.haile <mikias.haile@accenture.com>
2023-09-14 13:46:28 -0400

    building component layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+const initialMeals = [
+  { id: 1, description: "Scrumbled Eggs", type: "breakfast", eaten: true },
+  { id: 2, description: "Orange Juice", type: "breakfast", packed: false },
+  { id: 3, description: "Coffee", type: "breakfast", packed: false },
+];
+
+const mealTypes = ["Breakfast", "Lunch", "Snack", "Dinner"];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <MealList />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1>🫔 Meal Plan 🍱</h1>;
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you want to eat today</h3>
+      <select>
+        <option value={null} label="Select meal type"></option>
+        {mealTypes.map((type, index) => (
+          <option value={type} label={type} key={index}></option>
+        ))}
+      </select>
+      <input type="text" placeholder="meal description" />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function MealList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialMeals.map((meal) => (
+          <Meal meal={meal} key={meal.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Meal({ meal }) {
+  return (
+    <li>
+      <span style={meal.eaten ? { textDecoration: "line-through" } : {}}>
+        {meal.type}: {meal.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <em>
+      <footer className="stats">You have icluded x meals for the day</footer>
+    </em>
   );
 }
 

70a83ec26fe2d39cfcc1643d945c325b7e3c21a9
mikias.haile <mikias.haile@accenture.com>
2023-09-14 14:46:19 -0400

    controlled elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialMeals = [
   { id: 1, description: "Scrumbled Eggs", type: "breakfast", eaten: true },
   { id: 2, description: "Orange Juice", type: "breakfast", packed: false },
@@ -7,11 +9,12 @@ const initialMeals = [
 const mealTypes = ["Breakfast", "Lunch", "Snack", "Dinner"];
 
 function App() {
+  const [meals, setMeals] = useState(initialMeals);
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <MealList />
+      <Form mealList={meals} setMeals={setMeals} />
+      <MealList mealList={meals} />
       <Stats />
     </div>
   );
@@ -21,27 +24,42 @@ function Logo() {
   return <h1>🫔 Meal Plan 🍱</h1>;
 }
 
-function Form() {
+function Form({ mealList, setMeals }) {
+  const [mealDesc, setMealDesc] = useState("");
+  const [mealType, setMealType] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newMeal = { description: mealDesc, type: mealType, id: Date.now() };
+
+    setMeals([...mealList, newMeal]);
+  }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you want to eat today</h3>
-      <select>
-        <option value={null} label="Select meal type"></option>
+      <select value={mealType} onChange={(e) => setMealType(e.target.value)}>
+        <option value="" label="Select meal type"></option>
         {mealTypes.map((type, index) => (
           <option value={type} label={type} key={index}></option>
         ))}
       </select>
-      <input type="text" placeholder="meal description" />
+      <input
+        type="text"
+        placeholder="meal description"
+        value={mealDesc}
+        onChange={(e) => setMealDesc(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
 }
 
-function MealList() {
+function MealList({ mealList }) {
   return (
     <div className="list">
       <ul>
-        {initialMeals.map((meal) => (
+        {mealList.map((meal) => (
           <Meal meal={meal} key={meal.id} />
         ))}
       </ul>
